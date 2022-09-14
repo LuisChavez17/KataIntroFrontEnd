62558b6459df8fc6af8ccdd3b1dd26ca39240c7e
Luis Chavez <goldenchavez17@gmail.com>
2022-09-14 15:11:24 -0500

    feat: file webpack.config.js was modifeid to add loaders for babel and testear files js further exclude carpet node_modules and also added file loader for files png, jpg, svg, gif etc

--- webpack.config.js
+++ webpack.config.js
@@ -11,23 +11,41 @@ module.exports = {
                 use: [
                     {
                         loader: "html-loader",
-                        options: {minimize: true}
+                        options: { minimize: true }
                     }
                 ]
             },
             {
                 test: /.(css|scss)$/,
-               /* Telling webpack to use the style-loader, css-loader, and sass-loader to bundle the
-               css and scss files. */
+                /* Telling webpack to use the style-loader, css-loader, and sass-loader to bundle the
+                css and scss files. */
                 use: [
                     "style-loader",
                     "css-loader",
                     "sass-loader",
                 ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpg|svg|gif|jpeg)$/,
+                use: [
+                    "file-loader"
+                ]
             }
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: "./src/index.html"}),
+        new HtmlWebpackPlugin({ template: "./src/index.html" }),
         new MiniCssExtractPlugin()],
 }
