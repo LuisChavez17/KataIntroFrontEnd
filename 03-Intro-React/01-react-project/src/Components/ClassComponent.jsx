d2b52a4a9dc24ed1a2361e0025d5b9bf2399d0e3
Luis Chavez <goldenchavez17@gmail.com>
2022-09-22 08:51:48 -0500

    docs: Carpet Component was created to work and filles classcomponent.jsx and functioncomponent.jsx were created

---
+++ 03-Intro-React/01-react-project/src/Components/ClassComponent.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+/** 
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+*/
+
+/* The class component is a React component that extends the React.Component class. */
+class ClassComponent extends React.Component {
+    render() {
+        return {
+            <div className="app">
+            <h2>This is a class component</h2>
+            </div>
+        }
+    }
+}
+
+/* Exporting the ClassComponent class. */
+export default ClassComponent
