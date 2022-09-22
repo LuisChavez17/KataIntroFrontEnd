8977465ef2ca8d8299354c3a4266dd936a0493d4
Luis Chavez <goldenchavez17@gmail.com>
2022-09-22 16:20:54 -0500

    feat: file functioncomponent.jsx was modified to create an example of useState on functional components and create event to use the state and modified the state

--- 03-Intro-React/01-react-project/src/Components/FunctionComponent.jsx
+++ 03-Intro-React/01-react-project/src/Components/FunctionComponent.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const string = "tiene"
 
 const FunctionalComponent = () => {
+    const [person, setPerson] = useState({
+        name: 'Estefania',
+        rol: 'Frontend',
+    })
+
+    const [age, setAge] = useState(20)
+
     return (
-        <h2>hi from a functioncomponent</h2>
+        <>
+            <h2>hi from a functioncomponent: {string}{age}</h2>
+            <button onClick={(event) => { setAge(age + 1) }} >Click</button>
+            <button onClick={(event) => { setPerson(person.name) }} >Click</button>
+        </>
     )
 }
 
