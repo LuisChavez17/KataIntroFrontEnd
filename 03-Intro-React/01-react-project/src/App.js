da65de59212ba637c7820a7b2f3f0a030595dea3
Luis Chavez <goldenchavez17@gmail.com>
2022-09-22 08:54:20 -0500

    feat: file App.js was modified to import react from react and import class component from classcomponent.jsx

--- 03-Intro-React/01-react-project/src/App.js
+++ 03-Intro-React/01-react-project/src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import React from "react";
+import ClassComponent from "./Components/ClassComponent";
+
 
 function App() {
   return (
@@ -21,5 +25,39 @@ function App() {
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+/**
+ * It returns a div with a header, an image, a paragraph, and a link
+ * @returns The return statement is used to return from a function with a value. If the expression is
+ * omitted, undefined is returned instead.
+ */
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
