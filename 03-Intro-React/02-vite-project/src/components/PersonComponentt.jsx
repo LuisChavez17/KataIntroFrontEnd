3ce9d8fbb118af5561d888aaefb54fa8c6c7fd76
Luis Chavez <goldenchavez17@gmail.com>
2022-09-25 21:54:07 -0500

    docs: new components were created to export to app.jsx

---
+++ 03-Intro-React/02-vite-project/src/components/PersonComponentt.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class PersonComponentt extends Component {
+  render() {
+    return (
+      <div>PersonComponentt</div>
+    )
+  }
+}
---
+++ 03-Intro-React/02-vite-project/src/components/TeenagerComponent.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class TeenagerComponent extends Component {
+  render() {
+    return (
+      <div>TeenagerComponent</div>
+    )
+  }
+}
