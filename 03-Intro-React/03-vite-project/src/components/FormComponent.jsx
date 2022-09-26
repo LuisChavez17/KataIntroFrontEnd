faefb72ba38f7e2bbce240f80f399ddae7f5309b
Luis Chavez <goldenchavez17@gmail.com>
2022-09-26 16:45:45 -0500

    feat: file FormCommponent.jsx was modified to maquetar form component

--- 03-Intro-React/03-vite-project/src/components/FormComponent.jsx
+++ 03-Intro-React/03-vite-project/src/components/FormComponent.jsx
@@ -3,7 +3,27 @@ import React, { Component } from 'react'
 export class FormComponent extends Component {
   render() {
     return (
-      <div>FormComponent</div>
+      <div>
+        <h2>Crear Badge</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="firstName">Primer Nombre</label>
+            <input type="text" name="firstName" className="form-control" onChange={this.props.handleChange}/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="lastName">Apellido</label>
+            <input type="text" name="lastName" className="form-control" onChange={this.props.handleChange}/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="jobTitle">Rol</label>
+            <input type="text" name="jobTitle" className="form-control" onChange={this.props.handleChange}/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" className="form-control" onChange={this.props.handleChange}/>
+          </div>
+        </form>
+      </div>
     )
   }
 }
