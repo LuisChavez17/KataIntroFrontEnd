30da2672f3e33d8850f537d9a20f60f1c1def31f
Luis Chavez <goldenchavez17@gmail.com>
2022-09-25 20:21:06 -0500

    docs: carpet componets was created to create components to use them on app.jsx

---
+++ 03-Intro-React/02-vite-project/src/components/ChildComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ChildComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h3></h3>
+        ChildComponent</div>
+    )
+  }
+}
