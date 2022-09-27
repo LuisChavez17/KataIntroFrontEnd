dcd015d57d3df5e6649659e2f84fb863e0e592ee
Luis Chavez <goldenchavez17@gmail.com>
2022-09-27 16:53:14 -0500

    feat: project 04 was modified to replicate exactly the project 03 to work with badge with class components at this time all code was copied to this porject to modified and work with functional components

--- 03-Intro-React/04-vite-react-functional/src/App.jsx
+++ 03-Intro-React/04-vite-react-functional/src/App.jsx
@@ -1,34 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { Component } from 'react'
+import BadgeComponent from './components/BadgeComponent'
+import FormComponent from './components/FormComponent'
+import NavbarComponent from './components/NavbarComponent'
 
-function App() {
-  const [count, setCount] = useState(0)
+export class App extends Component {
+  state = {
+    form: {
+      firstName: "",
+      lastName: "",
+      username: "",
+      jobTitle: "",
+    }
+  }
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  handleChange = (event) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NavbarComponent/>
+        <FormComponent handleChange={this.handleChange}/>
+        <BadgeComponent
+        firstName={this.state.form.firstName} 
+        lastName={this.state.form.lastName}
+        username={this.state.form.username}
+        jobTitle={this.state.form.jobTitle}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <div className="App">
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src="/vite.svg" className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://reactjs.org" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </div>
+//   )
+// }
+
+// export default App
