1424c32e5b3b6b16c61f49619bc6679186db4975
Luis Chavez <goldenchavez17@gmail.com>
2022-09-28 14:15:22 -0500

    feat: LoginForm.jsx  was modified to add inputs and function handle onChange and use state with const LoginForm

--- 04-Reactfunctional-Parte2/01-reactfunctional-login/src/components/LoginForm.jsx
+++ 04-Reactfunctional-Parte2/01-reactfunctional-login/src/components/LoginForm.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const LoginForm = () => {
-  return (
-    <div>LoginForm Component</div>
-  )
-}
+    const [credentials, setCredentials] = useState({});
+
+    /**
+     * Tener dos inputs para el user y password
+     * & un boton para validar credenciales,
+     * Si las credenciales son correctas, dar acceso
+     *
+     * Harcodear 👀 credenciales con
+     * user: usuarioG17
+     * password: password
+     *
+     *
+     */
+
+    const handleFormValue = (e) => {
+        console.log(e.target.value);
+        // key: value
+        setCredentials();
+    };
+
+    return (
+        <div>
+            LoginForm Component
+            <div>
+                <input type="text" name="user" value={credentials.user} onChange={handleFormValue}/>
+                <input type="password" name="password" value={credentials.password} onChange={handleFormValue}/>
+            </div>
+        </div>
+    );
+};
+
 
 export default LoginForm
