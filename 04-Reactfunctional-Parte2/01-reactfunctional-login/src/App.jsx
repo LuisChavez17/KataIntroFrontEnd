241e0b66939e42bf76ba1ed0534fabf5ac2a5ba2
Luis Chavez <goldenchavez17@gmail.com>
2022-09-28 15:59:09 -0500

    feat: app.jsx was modified to create a conditional renderin over component loginform to validate if userlogged send true on value, will show elemnt h2 if not the state on const isUserLogged was initialized on value false waiting the vaue tru to be seteado

--- 04-Reactfunctional-Parte2/01-reactfunctional-login/src/App.jsx
+++ 04-Reactfunctional-Parte2/01-reactfunctional-login/src/App.jsx
@@ -3,12 +3,18 @@ import './App.css'
 import LoginForm from './components/LoginForm'
 
 function App() {
-
+  const [isUserLogged, setisUserLogged] = useState(false);
+  
   return (
     <div className="App">
       <h3>React Login 👤</h3>
       <div className="card">
         < LoginForm />
+        {!isUserLogged ? (
+          <LoginForm userLogged={(value) => setisUserLogged(value)} />
+        ) : (
+          <h2>Componente login exitoso</h2>
+        )}
       </div>
     </div>
   )
