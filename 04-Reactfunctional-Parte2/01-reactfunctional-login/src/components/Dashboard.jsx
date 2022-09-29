4a73d47e4d98c6cb51c4e34545585da25d4433c6
Luis Chavez <goldenchavez17@gmail.com>
2022-09-29 13:18:05 -0500

    feat: dashboard was modified to create a list element to show movies iterating on planetarray with method map which return a new arrary, and event onClick was added to call a function recoverPlanetDetail which return API info again when click over card

--- 04-Reactfunctional-Parte2/01-reactfunctional-login/src/components/Dashboard.jsx
+++ 04-Reactfunctional-Parte2/01-reactfunctional-login/src/components/Dashboard.jsx
@@ -18,7 +18,9 @@ const Dashboard = () => {
             .catch((error) => console.log("error calling SWAPI"));
     },/* An array of dependencies. If you pass an empty array, it will only run once when the component
     mounts. */
-     []);
+        []);
+
+    const recoverPlanetDetail = () => console.log("llamada otra vez a la API ");
 
     return (
         <>
@@ -29,10 +31,20 @@ const Dashboard = () => {
                 <h3>Cargando información... ⭕️</h3>
             ) : (
                 planetArray.map((planet, index) => (
-                    <div key={index} style={{ border: "2px solid white" }}>
+                    <div key={index} onClick={recoverPlanetDetail} style={{ border: "2px solid white", margin: "10px 0" }}>
                         <p>Name: {planet.name}</p>
                         <p>Climate {planet.climate}</p>
                         {/* <p> Lista de peliculas: <array> </p>  ???? */}
+                        <div>
+                            {" "}
+                            <b>Lista de peliculas: </b>
+                            <ul>
+                                {" "}
+                                {planet.films.map((planet, i) => (
+                                    <li key={i}> URL: {planet} </li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
                 ))
             )}
