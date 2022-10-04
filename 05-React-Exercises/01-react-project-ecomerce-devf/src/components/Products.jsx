d249474860dd4c7b4f5b0c03885f752c125aa321
Luis Chavez <goldenchavez17@gmail.com>
2022-10-04 00:53:26 -0500

    feat: call to API is working but API have some errors on data like name and description

--- 05-React-Exercises/01-react-project-ecomerce-devf/src/components/Products.jsx
+++ 05-React-Exercises/01-react-project-ecomerce-devf/src/components/Products.jsx
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+// import PlanetDetail from "./PlanetDetail";
 
-const Products = () => {
+const DashboardProducts = () => {
+	/* A constant that is used to store the base URL of the API. */
+	const BASE_URL = 'https://ecomerce-master.herokuapp.com/api/v1/';
+
+	const [productsArray, setproductsArray] = useState([]);
+	// const [planetInfo, setPlanetInfo] = useState({});
+	// const [planetIndex, setplanetIndex] = useState(undefined);
+
+	useEffect(() => {
+		axios
+			.get(`${BASE_URL}item`)
+			.then(({ data }) => setproductsArray(data))
+			.catch((error) => console.log(error,'error calling SWAPI'));
+	}, []);
+
+	// useEffect(() => {
+	//     if (planetIndex) {
+	//         axios
+	//             .get(`${BASE_URL}planets/${planetIndex}`)
+	//             .then(({ data }) => {
+	//                 console.log("planet details", data);
+	//                 setPlanetInfo(data);
+	//             })
+	//             .catch((error) => console.log("error calling SWAPI", error));
+	//     }
+	// }, [planetIndex]);
+
+	// const recoverPlanetDetail = (valorIndice) => {
+	//     setplanetIndex(valorIndice + 1);
+	// };
+
+
 	return (
-		<div>
-            
-			<button > buton </button>
-		</div>
+		<>
+			{productsArray.length === 0 ? (
+				<h3>Cargando información... ⭕️</h3>
+			) : (
+				productsArray.map((product, index) => (
+					<div key={index} style={{ border: '2px solid white' }}>
+						<p>Name: {product.product_name}</p>
+						<p>precio: {product.price}</p>
+					</div>
+				))
+			)}
+		</>
 	);
 };
 
-export default Products;
+export default DashboardProducts;
